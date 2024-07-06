54777da66ddea3fd73dc4c593218ac994f22ed8e
Craig Jones <clj3100@pm.me>
2024-07-06 19:21:48 -0400

    fixing network-online.target warning for syncoid service

--- customization/syncoid.nix
+++ customization/syncoid.nix
@@ -2,7 +2,7 @@
   systemd.services = {
     syncoid-nas = {
       description = "Syncoid backup to home TrueNAS";
-      after = [ "sanoid.service" "network-online.target" ];
+      after = [ "sanoid.service" ];
       wantedBy = [ "sanoid.service" ];
       serviceConfig = {
         ExecStart = "${pkgs.sanoid}/bin/syncoid --no-privilege-elevation --no-sync-snap --sshkey /var/lib/syncoid/backup rpool/home n16backup@192.168.1.8:First/Backup/nixos16";
