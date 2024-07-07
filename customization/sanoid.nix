deea345e8377be307ed497d3501a53d6bc6a2282
Craig Jones <clj3100@pm.me>
2024-07-07 12:51:01 -0400

    adding more paths for sanoid and syncoid

--- customization/sanoid.nix
+++ customization/sanoid.nix
@@ -8,8 +8,23 @@
       daily = 30;
       monthly = 1;
     };
+    templates.lessbackup = {
+      autoprune = true;
+      autosnap = true;
+      hourly = 0;
+      daily = 7;
+      monthly = 1;
+    };
     datasets."rpool/home" = {
       useTemplate = [ "backup" ];
+    };
+    
+    datasets."rpool/root" = {
+      useTemplate = [ "lessbackup" ];
+    };
+
+    datasets."rpool/vms" = {
+      useTemplate = [ "lessbackup" ];
     };
   };
 }
--- customization/syncoid.nix
+++ customization/syncoid.nix
@@ -4,9 +4,17 @@
       #user = "backupuser";
       commonArgs = ["--no-sync-snap" "--no-privilege-elevation"];
       sshKey = "/var/lib/syncoid/backup";
-      commands."truenas" = {
+      commands."truenas_home" = {
         source = "rpool/home";
         target = "n16backup@192.168.1.8:First/Backup/nixos16/home";
+      };
+      commands."truenas_root" = {
+        source = "rpool/root";
+        target = "n16backup@192.168.1.8:First/Backup/nixos16/root";
+      };
+      commands."truenas_vms" = {
+        source = "rpool/vms";
+        target = "n16backup@192.168.1.8:First/Backup/nixos16/vms";
       };
   };
 }
