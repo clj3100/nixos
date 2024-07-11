511be666ab2e696eafd9456863d734d42d440ff8
Craig Jones <clj3100@pm.me>
2024-07-11 12:34:04 -0400

    changing starship config

--- home/default.nix
+++ home/default.nix
@@ -31,8 +31,7 @@
     enable = true;
     # custom settings
     settings = {
-      add_newline = false;
-      aws.disabled = true;
+      add_newline = true;
       gcloud.disabled = true;
       line_break.disabled = true;
     };
