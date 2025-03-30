fa1b8cfaf7fb13cf9b6cb6d9c16666c8c3fcfe17
Craig Jones <clj3100@pm.me>
2025-03-30 14:53:23 -0400

    modifying xr for steamvr

--- customization/xr.nix
+++ customization/xr.nix
@@ -1,8 +1,13 @@
 {pkgs, ...}:{
 
-services.monado = {
-  enable = true;
-  defaultRuntime = true;
-};
+  services.monado = {
+    enable = true;
+    defaultRuntime = true;
+  };
+
+  systemd.user.services.monado.environment = {
+    STEAMVR_LH_ENABLE = "1";
+    XRT_COMPOSITOR_COMPUTE = "1";
+  };
 
 }
